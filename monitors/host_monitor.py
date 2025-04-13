2af1d7e28aef23b962501651380628f79485f2a1
ansongdkucl <121667959+ansongdkucl@users.noreply.github.com>
2025-04-13 04:50:31 +0100

    Update host_monitor.py

--- monitors/host_monitor.py
+++ monitors/host_monitor.py
@@ -1,12 +1,24 @@
-import subprocess
+import scapy.all as scapy
 
-def discover_hosts_with_arp_scan(subnet):
-    print(f"\nDiscovering hosts on {subnet} using arp-scan...")
-    result = subprocess.run(
-        ['sudo', 'arp-scan', subnet],
-        capture_output=True,
-        text=True
-    )
-    print(result.stdout)
 
-discover_hosts_with_arp_scan('192.168.1.0/24')
+def discover_hosts(subnet_address):
+    
+    print(f'\n---------Discovering hosts on {subnet_address} ----------')
+    ans, unans  = scapy.arping(subnet_address)
+    ans.summary()
+    print(ans)
+
+def main():
+
+    #subnet_address = '192.168.1.0/24'
+    subnet_address = "172.17.57.240/32"
+    discover_hosts(subnet_address)
+    
+
+if __name__== "__main__":
+    try:
+    
+        main()
+
+    except KeyboardInterrupt:
+        print("\n\nExiting monitor")
